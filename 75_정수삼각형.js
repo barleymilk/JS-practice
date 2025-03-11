f256a37c81975cd2615994b50adeb5fb20f3434d
barleymilk <barleymilk640@gmail.com>
2025-03-11 13:26:12 +0900

    commit

--- 75_정수삼각형.js
+++ 75_정수삼각형.js
@@ -1,5 +1,6 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/43105
 
+/*
 function solution(triangle) {
   const n = triangle.length;
   const dp = Array.from(Array(n), () => Array(n).fill(0));
@@ -15,6 +16,19 @@ function solution(triangle) {
   }
 
   return dp[0][0]; // 꼭대기 최댓값 반환
+}
+*/
+
+function solution(triangle) {
+  const n = triangle.length;
+
+  for (let i = n - 2; i >= 0; i--) {
+    for (let j = 0; j <= i; j++) {
+      triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+    }
+  }
+
+  return triangle[0][0]; // 최댓값 반환
 }
 
 console.log(solution([[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]));
