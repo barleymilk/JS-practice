ebf9e59db8cfc0ae59506f70d53894d4c8e76742
barleymilk <barleymilk640@gmail.com>
2025-03-21 15:28:18 +0900

    commit

---
+++ 78_가장큰정사각형찾기.js
@@ -0,0 +1,28 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12905
+
+function solution(board) {
+  const ROW = board.length;
+  const COL = board[0].length;
+
+  for (let i = 1; i < ROW; i++) {
+    for (let j = 1; j < COL; j++) {
+      if (board[i][j] === 1) {
+        const up = board[i - 1][j];
+        const left = board[i][j - 1];
+        const upLeft = board[i - 1][j - 1];
+        board[i][j] = Math.min(up, left, upLeft) + 1;
+      }
+    }
+  }
+  const maxVal = Math.max(...board.map((row) => Math.max(...row)));
+  return maxVal * maxVal;
+}
+
+console.log(
+  solution([
+    [0, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [0, 0, 1, 0],
+  ])
+);
